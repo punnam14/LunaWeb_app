7f1e9f3a186a6cfaa72a5e58fc0793d530b0c80c
P.S <1998punnam@gmail.com>
2023-08-15 14:51:56 -0700

    Prepare for Heroku deployment

--- modules/config.js
+++ modules/config.js
@@ -1,4 +1,12 @@
+// module.exports = {
+//     HOST: 'localhost',
+//     PORT: 3000,
+// };
+
+const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0'; 
+
 module.exports = {
-    HOST: 'localhost',
-    PORT: 3000,
+  PORT,
+  HOST,
 };
