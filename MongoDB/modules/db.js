604e8b6946a5a492cc41b1f9cb2e0aab145bcd58
P.S <1998punnam@gmail.com>
2023-08-15 15:23:53 -0700

    Increase mongoose timeout

--- MongoDB/modules/db.js
+++ MongoDB/modules/db.js
@@ -3,7 +3,7 @@ const { mongodb } = require('./credentials.js');
 
 function connectDB(connect = true) {
     if (connect) {
-        mongoose.connect(mongodb.connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+        mongoose.connect(mongodb.connectionString, { useNewUrlParser: true, useUnifiedTopology: true,  serverSelectionTimeoutMS: 50000})
         .then(() => console.log('Connected to MongoDB'))
         .catch(error => console.error('Failed to connect to MongoDB', error));
     } else {
